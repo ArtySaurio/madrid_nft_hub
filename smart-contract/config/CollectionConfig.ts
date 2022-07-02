34d911f2b4bf084c6549ac35e711c8db45178d57
ArtySaurio <arturito100500@gmail.com>
2022-07-02 18:23:54 +0200

    Done, with contract in 0x3d16670741efEe366f30392cFfcC8D939DD60a86.

--- smart-contract/config/CollectionConfig.ts
+++ smart-contract/config/CollectionConfig.ts
@@ -9,24 +9,24 @@ const CollectionConfig: CollectionConfigInterface = {
   // The contract name can be updated using the following command:
   // yarn rename-contract NEW_CONTRACT_NAME
   // Please DO NOT change it manually!
-  contractName: 'YourNftToken',
-  tokenName: 'My NFT Token',
-  tokenSymbol: 'MNT',
-  hiddenMetadataUri: 'ipfs://__CID__/hidden.json',
-  maxSupply: 10000,
+  contractName: 'MadridNFTHub',
+  tokenName: 'Madrid NFT Hub',
+  tokenSymbol: 'MNFT',
+  hiddenMetadataUri: 'ipfs://QmPQAW5XjAFtas42NTHqYFxDS1cBN39G7tWgynjHe7yz15/hidden.json',
+  maxSupply: 1000,
   whitelistSale: {
-    price: 0.05,
+    price: 0.03,
     maxMintAmountPerTx: 1,
   },
   preSale: {
-    price: 0.07,
+    price: 0.05,
     maxMintAmountPerTx: 2,
   },
   publicSale: {
-    price: 0.09,
+    price: 0.07,
     maxMintAmountPerTx: 5,
   },
-  contractAddress: null,
+  contractAddress: "0x3d16670741efEe366f30392cFfcC8D939DD60a86",
   marketplaceIdentifier: 'my-nft-token',
   marketplaceConfig: Marketplaces.openSea,
   whitelistAddresses,
